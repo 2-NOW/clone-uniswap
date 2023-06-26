5c7f31311440eca1a315bfb4ee961b431424ae3b
2-NOW <kj109888@gmail.com>
2023-06-27 02:40:18 +0900

    Add node environment to ESLint config and include recommended plugins for TailwindCSS, React, and Prettier, while disabling certain rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,32 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    "plugin:tailwindcss/recommended",
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    "prettier"
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import', 'react'],
   rules: {
+    'react/no-unknown-property': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
     'react-refresh/only-export-components': 'warn',
+    "import/newline-after-import": "warn",
+    "tailwindcss/no-custom-classname": "off",
+    "import/order": [
+      "warn",
+      {
+        "newlines-between": "always",
+        "alphabetize": {
+          "order": "asc",
+          "caseInsensitive": true
+        }
+      }
+    ],
   },
 }
