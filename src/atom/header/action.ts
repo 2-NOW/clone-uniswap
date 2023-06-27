9fc16864e5a221b829732137dcb166c1dd4cf331
2-NOW <kj109888@gmail.com>
2023-06-27 14:53:29 +0900

    Add atom for toggling the state of IS_MORE_OPEN in header

---
+++ src/atom/header/action.ts
@@ -0,0 +1,7 @@
+import { atom } from "jotai";
+
+import { IS_MORE_OPEN } from "./state";
+
+export const TOGGLE_MORE = atom(null, (get, set) => {
+  set(IS_MORE_OPEN, !get(IS_MORE_OPEN));
+});
---
+++ src/atom/header/state.ts
@@ -0,0 +1,3 @@
+import { atom } from "jotai";
+
+export const IS_MORE_OPEN = atom(false);
