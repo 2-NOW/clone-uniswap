dce5e9c07329a40eae55f60445774bc07ae0f3d3
2-NOW <kj109888@gmail.com>
2023-06-27 14:52:42 +0900

    Add menu component with clickable links and a "More" button for additional options

---
+++ src/components/layout/header/left/menu/index.tsx
@@ -0,0 +1,41 @@
+import { ListItem } from "./list-item";
+import { MoreButton } from "./more-button";
+
+export const Menu = () => {
+  // TODO: add hover effect
+  // TODO: add active effect
+
+  return (
+    <ul className="flex items-center">
+      {[
+        {
+          label: "Swap",
+          href: "/",
+        },
+        {
+          label: "Tokens",
+          href: "https://app.uniswap.org/#/tokens/ethereum_goerli",
+        },
+        {
+          label: "NFTs",
+          href: "https://app.uniswap.org/#/nfts",
+        },
+      ].map(({ label, href }) => (
+        <ListItem
+          key={`${label}-${href}`}
+          label={label}
+          href={href}
+          isExternal={href.includes("https://")}
+          style={{
+            list: "my-1 px-3.5 py-2 font-medium",
+            link: "text-white hover:text-gray-400",
+          }}
+        />
+      ))}
+
+      <li className="mr-1">
+        <MoreButton />
+      </li>
+    </ul>
+  );
+};
