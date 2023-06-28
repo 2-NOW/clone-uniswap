b7d7772e48e2bc66b170c9f58bfa655537390f96
2-NOW <kj109888@gmail.com>
2023-06-28 21:49:09 +0900

    Add new svg file for swapping currency

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -2,3 +2,4 @@ export { ReactComponent as LogoSvg } from "./logo.svg";
 export { ReactComponent as DotsSvg } from "./dots.svg";
 export { ReactComponent as SettingSvg } from "./setting.svg";
 export { ReactComponent as ArrowDownSvg } from "./arrow-down.svg";
+export { ReactComponent as SwapInputSvg } from "./swap-currency.svg";
