f1d7c1464b048a8a671289eda148018c16878c08
2-NOW <kj109888@gmail.com>
2023-06-28 21:50:51 +0900

    Add setting menu to Swap header

---
+++ src/components/app/main/swap/header/setting.tsx
@@ -0,0 +1,10 @@
+import { SettingSvg } from "@/assets/svgs";
+
+export const Setting = () => {
+  // TODO: add setting menu
+  return (
+    <div className="hover:text-gray-400/70">
+      <SettingSvg />
+    </div>
+  );
+};
