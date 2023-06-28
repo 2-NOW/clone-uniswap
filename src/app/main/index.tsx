e7f25714565e1e6457724522fb33cce0d9357e16
2-NOW <kj109888@gmail.com>
2023-06-28 21:49:48 +0900

    Add Swap component to Main component in the main/index.tsx file

--- src/app/main/index.tsx
+++ src/app/main/index.tsx
@@ -1,8 +1,10 @@
 import { BlockSyncStatus } from "@/components/app/main/block-status";
+import { Swap } from "@/components/app/main/swap";
 
 export const Main = () => {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col items-center pb-20 pt-[72px]">
+      <Swap />
       <BlockSyncStatus />
     </div>
   );
