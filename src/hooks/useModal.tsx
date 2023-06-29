a58d1cd5ec8f3b634f8ae3179ea223830c34ecb1
2-NOW <kj109888@gmail.com>
2023-06-29 10:07:55 +0900

    Create a new file "useModal.tsx" that exports a custom hook "useModal" which sets and removes modals using Jotai, and returns a modal object

---
+++ src/hooks/useModal.tsx
@@ -0,0 +1,31 @@
+import { useSetAtom } from "jotai";
+
+import { Dialog } from "@/components/modal/template";
+import { REMOVE_MODAL, SET_MODAL } from "@/state/modal/action";
+
+export const useModal = () => {
+  const set = useSetAtom(SET_MODAL);
+  const remove = useSetAtom(REMOVE_MODAL);
+
+  const modal = (modal: JSX.Element) => {
+    return new Promise((resolve) => {
+      const id = Math.random().toString(36).slice(-8);
+
+      const callback = (dialog: HTMLDialogElement) => {
+        if (!dialog || dialog?.open) return;
+        dialog.showModal();
+        dialog.addEventListener(
+          "close",
+          () => {
+            remove(id);
+            resolve(dialog.returnValue);
+          },
+          { once: true }
+        );
+      };
+      set(<Dialog callback={callback}>{modal}</Dialog>, id);
+    });
+  };
+
+  return { modal };
+};
