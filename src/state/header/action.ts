8dcb5362ab664a9b798cc5dafb83e05d5f94e988
2-NOW <kj109888@gmail.com>
2023-06-29 10:08:51 +0900

    Refactor import statement to use relative path in header action.ts and rename state.ts to index.ts in header directory

--- src/state/header/action.ts
+++ src/state/header/action.ts
@@ -1,6 +1,6 @@
 import { atom } from "jotai";
 
-import { IS_MORE_OPEN } from "./state";
+import { IS_MORE_OPEN } from ".";
 
 export const TOGGLE_MORE = atom(null, (get, set) => {
   set(IS_MORE_OPEN, !get(IS_MORE_OPEN));
