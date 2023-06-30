609f14586bbb25a0d7f6330a789e6e9e05581bf7
2-NOW <kj109888@gmail.com>
2023-07-01 03:01:17 +0900

    Add close and search SVG icons to the assets/svgs directory

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -3,3 +3,5 @@ export { ReactComponent as DotsSvg } from "./dots.svg";
 export { ReactComponent as SettingSvg } from "./setting.svg";
 export { ReactComponent as ArrowDownSvg } from "./arrow-down.svg";
 export { ReactComponent as SwapInputSvg } from "./swap-currency.svg";
+export { ReactComponent as CloseSvg } from "./close.svg";
+export { ReactComponent as SearchSvg } from "./search.svg";
