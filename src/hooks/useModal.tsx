805a4cfc04a9356fa72c2eb5dc5049f75f319309
2-NOW <kj109888@gmail.com>
2023-07-01 03:01:03 +0900

    Refactor useModal to include a close function and a new InitialProvider

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,7 +1,9 @@
 import { useSetAtom } from "jotai";
 
 import { Dialog } from "@/components/modal/template";
+import { CLOSE_MODAL } from "@/state/modal";
 import { REMOVE_MODAL, SET_MODAL } from "@/state/modal/action";
+import { InitialProvider } from "@/state/util/provider";
 
 export const useModal = () => {
   const set = useSetAtom(SET_MODAL);
@@ -10,6 +12,7 @@ export const useModal = () => {
   const modal = (modal: JSX.Element) => {
     return new Promise((resolve: (value: string) => void) => {
       const id = Math.random().toString(36).slice(-8);
+      let _dialog: HTMLDialogElement | null = null;
 
       const callback = (dialog: HTMLDialogElement) => {
         if (!dialog || dialog?.open) return;
@@ -22,9 +25,20 @@ export const useModal = () => {
           },
           { once: true }
         );
+        _dialog = dialog;
       };
 
-      set(<Dialog callback={callback}>{modal}</Dialog>, id);
+      const close = () => {
+        if (!_dialog) return;
+        _dialog.close();
+      };
+
+      set(
+        <InitialProvider values={[[CLOSE_MODAL, { close }]]}>
+          <Dialog callback={callback}>{modal}</Dialog>
+        </InitialProvider>,
+        id
+      );
     });
   };
 
