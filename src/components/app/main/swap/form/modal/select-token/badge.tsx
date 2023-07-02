0d91a4b6118f4cbd8e62719da02a76cc40ffd5f9
2-NOW <kj109888@gmail.com>
2023-07-02 16:13:00 +0900

    Refactor CurrencyBadge component to use CurrencyLogo component for displaying currency logos and add onClick event handler for selecting currencies in BaseCurrencyList component

--- src/components/app/main/swap/form/modal/select-token/badge.tsx
+++ src/components/app/main/swap/form/modal/select-token/badge.tsx
@@ -1,37 +1,28 @@
 import clsx from "clsx";
 
+import { CurrencyLogo } from "@/components/global/logo";
 import { getBaseCurrencies } from "@/constants/base";
-import { getLogoSrc } from "@/constants/logo";
+import { Chain } from "@/state/chain";
 
 interface CurrencyBadgeProps {
   currency: string;
   selected: boolean;
+  onClick: () => void;
 }
 
-const CurrencyBadge = ({ currency, selected }: CurrencyBadgeProps) => {
-  const logoSrc = getLogoSrc(currency);
-
+const CurrencyBadge = ({ currency, selected, onClick }: CurrencyBadgeProps) => {
   return (
     <div
+      onClick={!selected ? onClick : undefined}
       className={clsx([
         "m-1 flex items-center rounded-2xl border p-1.5 pr-3 hover:bg-[#98A1C014]",
         selected
-          ? "border-[#4C82FB] text-[#4C82FB]"
+          ? "cursor-default border-[#4C82FB] text-[#4C82FB]"
           : "cursor-pointer border-[#98A1C03d]",
       ])}
     >
       <div className="mr-2">
-        {logoSrc ? (
-          <img
-            className="h-6 w-6 rounded-full"
-            alt={`${currency} logo`}
-            src={logoSrc}
-          />
-        ) : (
-          <div className="flex h-6 w-6 items-center justify-center rounded-full bg-gray-500">
-            <span className="text-xl font-semibold">{currency[0]}</span>
-          </div>
-        )}
+        <CurrencyLogo size="24px" currency={currency} />
       </div>
       <div>{currency}</div>
     </div>
@@ -39,13 +30,15 @@ const CurrencyBadge = ({ currency, selected }: CurrencyBadgeProps) => {
 };
 
 interface BaseCurrencyListProps {
-  chain: string;
+  chain: Chain;
   selectedCurrency: string | null;
+  onSelectCurrency: (currency: string) => void;
 }
 
 export const BaseCurrencyList = ({
   chain,
   selectedCurrency,
+  onSelectCurrency,
 }: BaseCurrencyListProps) => {
   const bases = getBaseCurrencies(chain);
 
@@ -54,6 +47,7 @@ export const BaseCurrencyList = ({
       {bases &&
         bases.map(({ symbol }) => (
           <CurrencyBadge
+            onClick={() => onSelectCurrency(symbol)}
             key={`badge-${symbol}`}
             currency={symbol}
             selected={selectedCurrency === symbol}
