13ef8a0c7715fc1cba6348a679e9e25b291131ea
2-NOW <kj109888@gmail.com>
2023-07-02 17:52:43 +0900

    Rename `SwapInput` component to `SwitchInput` and update import of SVG component in `src/components/app/main/swap/form/switch.tsx`.

--- src/components/app/main/swap/form/switch.tsx
+++ src/components/app/main/swap/form/switch.tsx
@@ -1,15 +1,15 @@
 import { HTMLAttributes } from "react";
 
-import { SwapInputSvg } from "@/assets/svgs";
+import { ArrowDown } from "@/assets/svgs";
 
-export const SwapInput = ({ ...props }: HTMLAttributes<HTMLDivElement>) => {
+export const SwitchInput = ({ ...props }: HTMLAttributes<HTMLDivElement>) => {
   return (
     <div
       {...props}
       className="relative z-[1] mx-auto my-[-18px] h-10 w-10 cursor-pointer rounded-xl border-4 border-[#0D111C] bg-[#293249] hover:bg-[#293249]/80"
     >
       <div className="flex h-full w-full items-center justify-center">
-        <SwapInputSvg />
+        <ArrowDown />
       </div>
     </div>
   );
