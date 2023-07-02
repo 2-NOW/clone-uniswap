cf5f6d167e9fb900fda34209516473a082238140
2-NOW <kj109888@gmail.com>
2023-07-02 14:10:27 +0900

    Refactor the "action.ts" file to use "ModalState" instead of "Modal" for improved clarity and consistency

--- src/state/modal/action.ts
+++ src/state/modal/action.ts
@@ -1,11 +1,11 @@
 import { atom } from "jotai";
 
-import { MODALS, Modal } from ".";
+import { MODALS, ModalState } from ".";
 
 export const SET_MODAL = atom(
   null,
   (_, set, modal: JSX.Element, id: string) => {
-    const _modal: Modal = { id, component: modal };
+    const _modal: ModalState = { id, component: modal };
     set(MODALS, (prev) => {
       if (prev) return [...prev, _modal];
       return [_modal];
