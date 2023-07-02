c6c7e38ecc3eb3f06e47f189e1df0012426fc0e3
2-NOW <kj109888@gmail.com>
2023-07-02 17:52:33 +0900

    Refactor arrow-down.svg and add chevron-down.svg and ArrowDown to index.ts

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,8 +1,8 @@
 export { ReactComponent as LogoSvg } from "./logo.svg";
 export { ReactComponent as DotsSvg } from "./dots.svg";
 export { ReactComponent as SettingSvg } from "./setting.svg";
-export { ReactComponent as ArrowDownSvg } from "./arrow-down.svg";
-export { ReactComponent as SwapInputSvg } from "./swap-currency.svg";
+export { ReactComponent as ChevronDown } from "./chevron-down.svg";
+export { ReactComponent as ArrowDown } from "./arrow-down.svg";
 export { ReactComponent as CloseSvg } from "./close.svg";
 export { ReactComponent as SearchSvg } from "./search.svg";
 export { ReactComponent as CheckSvg } from "./check.svg";
