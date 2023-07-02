dc49337cf66d453c5f5c542f61331e7125708e52
2-NOW <kj109888@gmail.com>
2023-07-02 16:12:04 +0900

    Add new SVG file for check mark icon

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -5,3 +5,4 @@ export { ReactComponent as ArrowDownSvg } from "./arrow-down.svg";
 export { ReactComponent as SwapInputSvg } from "./swap-currency.svg";
 export { ReactComponent as CloseSvg } from "./close.svg";
 export { ReactComponent as SearchSvg } from "./search.svg";
+export { ReactComponent as CheckSvg } from "./check.svg";
