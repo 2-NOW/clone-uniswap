b81e70008aad5576d1aaec1188ac0181367f3b23
2-NOW <kj109888@gmail.com>
2023-07-02 16:12:29 +0900

    Refactor CurrencyList component to handle no results found and to highlight the selected currency

--- src/components/app/main/swap/form/modal/select-token/list.tsx
+++ src/components/app/main/swap/form/modal/select-token/list.tsx
@@ -1,26 +1,65 @@
+import clsx from "clsx";
+
+import { CheckSvg } from "@/assets/svgs";
+import { CurrencyLogo } from "@/components/global/logo";
 import { BaseCurrency } from "@/constants/base";
 
-const CurrencyRow = ({ name, symbol }: BaseCurrency) => {
+interface CurrencyRowProps extends BaseCurrency {
+  disabled?: boolean;
+  onClick: () => void;
+}
+
+const CurrencyRow = ({ name, symbol, disabled, onClick }: CurrencyRowProps) => {
   return (
-    <div className="px-5 py-1">
-      <div className="flex flex-col">
-        <div>{name}</div>
-        <div>{symbol}</div>
+    <div
+      onClick={!disabled ? onClick : undefined}
+      className={clsx([
+        "flex h-14 items-center gap-4 px-5 py-1 hover:bg-[#98A1C014]",
+        disabled
+          ? "pointer-events-none cursor-default opacity-40"
+          : "cursor-pointer",
+      ])}
+    >
+      <div>
+        <CurrencyLogo size="36px" currency={symbol} />
       </div>
+      <div className="flex grow flex-col">
+        <div>{name}</div>
+        <div className="text-xs font-light text-[#5D6785]">{symbol}</div>
+      </div>
+      {disabled && <CheckSvg className="h-5 w-5 text-[#4C82FB]" />}
     </div>
   );
 };
 
 interface CurrencyListProps {
   currencies: Array<BaseCurrency>;
+  selectedCurrency: string | null;
+  onSelectCurrency: (currency: string) => void;
 }
 
-export const CurrencyList = ({ currencies }: CurrencyListProps) => {
-  // TODO: no result found
+export const CurrencyList = ({
+  currencies,
+  selectedCurrency,
+  onSelectCurrency,
+}: CurrencyListProps) => {
+  if (currencies.length === 0) {
+    return (
+      <div className="h-full p-5 text-center">
+        <span>No results found</span>
+      </div>
+    );
+  }
+
   return (
     <div className="h-full">
       {currencies.map((currency) => (
-        <CurrencyRow key={currency.symbol} {...currency} />
+        <CurrencyRow
+          key={currency.symbol}
+          onClick={() => onSelectCurrency(currency.symbol)}
+          disabled={selectedCurrency === currency.symbol}
+          {...currency}
+        />
       ))}
     </div>
   );
