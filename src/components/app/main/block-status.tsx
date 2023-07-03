9ade5897c3c56c2c17286ffeb07a0ee14e16132f
2-NOW <kj109888@gmail.com>
2023-07-03 16:00:24 +0900

    Add BlockSyncStatus component with block sync status and polling

---
+++ src/components/app/main/block-status.tsx
@@ -0,0 +1,10 @@
+export const BlockSyncStatus = () => {
+  // TODO: add block sync status
+  // TODO: polling block sync
+  return (
+    <div className="fixed bottom-0 right-0 flex items-center gap-2 p-4">
+      <span className="text-xs text-[#3F6D55]">17611801</span>
+      <div className="h-2 w-2 rounded-full bg-[#76D191]" />
+    </div>
+  );
+};
