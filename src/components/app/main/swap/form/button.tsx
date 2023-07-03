38349eeb27240754fc5bcffd87a3814bfff55fa5
2-NOW <kj109888@gmail.com>
2023-07-03 15:07:40 +0900

    Refactor SwapButton component to use a separate Button component and add TODOs for sidebar interaction and swap action

--- src/components/app/main/swap/form/button.tsx
+++ src/components/app/main/swap/form/button.tsx
@@ -5,7 +5,7 @@ interface SwapButtonProps
   label: string;
 }
 
-export const SwapButton = ({ label, ...props }: SwapButtonProps) => {
+const Button = ({ label, ...props }: SwapButtonProps) => {
   return (
     <button
       className="w-full rounded-[20px] border border-transparent bg-[#4C82FB3d] p-4 text-center transition duration-200 hover:bg-[#21355A]"
@@ -15,4 +15,10 @@ export const SwapButton = ({ label, ...props }: SwapButtonProps) => {
       <div className="text-xl font-semibold text-[#4C82FB]">{label}</div>
     </button>
   );
+};
+
+export const SwapButton = () => {
+  // TODO: sidebar interaction
+  // TODO: swap action
+  return <Button label="Connect Wallet" />;
 };
