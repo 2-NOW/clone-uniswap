fe6b28831df66b24d4e4aa46f29aaf633dce223d
2-NOW <kj109888@gmail.com>
2023-07-03 14:54:59 +0900

    Add precise.ts to src/utils directory and implement currencyAmountPrecision function that returns the precision of a CurrencyAmount

---
+++ src/utils/precise.ts
@@ -0,0 +1,7 @@
+import { Currency, CurrencyAmount } from "@uniswap/sdk-core";
+
+export const currencyAmountPrecision = (amount?: CurrencyAmount<Currency>) => {
+  if (!amount) return undefined;
+  const floatNumber = parseFloat(amount.toExact());
+  return floatNumber < 0.1 ? parseFloat(amount.toSignificant(6)) : floatNumber;
+};
