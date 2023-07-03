5f0147b59f8cfe359745ea5fc04ead4284c5e392
2-NOW <kj109888@gmail.com>
2023-07-03 17:29:20 +0900

    Add resolve alias for "jsbi" to fix module resolution issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from "path";
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
@@ -6,4 +8,15 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      jsbi: path.resolve(
+        __dirname,
+        "node_modules",
+        "jsbi",
+        "dist",
+        "jsbi-cjs.js"
+      ),
+    },
+  },
 });
